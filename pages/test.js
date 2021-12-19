d99a9d7feebaad6d37b2cab95f3065bef58d96f6
David McGregor <david_mcgregor@homedepot.com>
2021-12-19 09:27:36 -0300

    Update

--- pages/test.js
+++ pages/test.js
@@ -2,51 +2,45 @@ import Link from 'next/link'
 import { Card, Typography, Space } from '@supabase/ui'
 import { supabase } from '../utils/initSupabase'
 
-export default function Profile({ user }) {
+export default function Profile({ data }) {
   return (
     <div style={{ maxWidth: '420px', margin: '96px auto' }}>
       <Card>
         <Space direction="vertical" size={6}>
-          <Typography.Text>You're signed in</Typography.Text>
-          <Typography.Text strong>Email: {user.email}</Typography.Text>
+          <Typography.Text>Instructions Page</Typography.Text>
           <Typography.Text type="success">
-            User data retrieved server-side (from Cookie in getServerSideProps):
+            This is a page that can be seen with a unique code. Without the code, this page cannot be visited without a code
           </Typography.Text>
 
           <Typography.Text>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
           </Typography.Text>
 
-          <Typography.Text>
-            <Link href="/">
-              <a>Static example with useSWR</a>
-            </Link>
-          </Typography.Text>
         </Space>
       </Card>
     </div>
   )
 }
 
-export async function getServerSideProps({ req }) {
+export async function getServerSideProps({ req, query }) {
   const REST_URL = 'http://localhost:3000'
   // const postgrest = new PostgrestClient(REST_URL, { fetch: fetch })
 
   const { data, error } = await supabase
   .from('users')
   .select()
-  .eq('code', '59485e2d-2bf5-4720-9832-46e0e6e8a27c')
+  .eq('code', query?.id)
 
-  console.log(data)
+  console.log(query)
 
 
-  const { user } = await supabase.auth.api.getUserByCookie(req)
+  //const { user } = await supabase.auth.api.getUserByCookie(req)
 
-  if (!user) {
+  if (!data) {
     // If no user, redirect to index.
     return { props: {}, redirect: { destination: '/', permanent: false } }
   }
 
   // If there is a user, return it.
-  return { props: { user } }
+  return { props: { data } }
 }
